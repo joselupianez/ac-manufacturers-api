244473c08f9851e21648fa06600a36865f1c959d
Jose Lupianez <joselupianez@fullsail.edu>
2022-08-28 11:55:36 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+<div align="center">
+  <h3 align="center">Air Conditioning Manufacturers API</h3>
+  <p align="center">
+    An API with information on some of the biggest air conditioning companies.
+  </p>
+  <img src="screenshot.png">
+</div>
+
+## How It's Made:
+Stored large air conditioning manufacturers' data (brands, year founded, headquarters, key people, and website). Added two endpoints for quickly accessing the information of all companies or a single company.
+### Tech Used:
+[![JS][Javascript]][Javascript]
+
+<!-- GETTING STARTED -->
+## Installation
+
+2. Install NPM packages
+   ```sh
+   npm install
+   ```
+3. Start the development server
+   ```sh
+   npm run dev
+   ```
+4. Access server at [https://localhost:8000](https://localhost:8000)
+
+## Endpoints
+
+All Companies - [https://ac-manufacturers.herokuapp.com/api/](https://calm-fjord-71736.herokuapp.com/api)
+
+Specific Company - [https://ac-manufacturers.herokuapp.com/api/rheem](https://calm-fjord-71736.herokuapp.com/api/rheem)
+
+## Optimizations
+
+Added the ability to display all companies with one request. Endpoint for specific company works regardless of capitalization in paremeters. 
+
+## Lessons Learned:
+
+Finding correct, up-to-date information on some smaller air conditioning brands can be cumbersome due to their lack of online presence. I found their information by searching through news websites and brand trademarks. An API is a great way to keep content related to a specific topic in one place, making it easier for those researching the topic to get the information they need.
+
+<!-- MARKDOWN LINKS & IMAGES -->
+[Javascript]: https://img.shields.io/badge/javascript%20-%23323330.svg?&style=for-the-badge&logo=javascript&logoColor=%23F7DF1E
+[screenshot]: screenshot.png
---
+++ server.js
@@ -0,0 +1,128 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = 5000
+
+// Load environment variables from .env files.
+require('dotenv').config()
+
+app.use(cors())
+
+// Air Conditioning Manufacturers
+let manufacturers = {
+    'american standard': {
+        'name': 'American Standard',
+        'brands': ['American Standard'],
+        'founded': '1929',
+        'headquarters': 'Piscataway, New Jersey, U.S.',
+        'keyPeople': ['Frederic M. Poses'],
+        'website': 'https://www.americanstandardair.com'
+    },
+    'carrier':{
+        'name': 'Carrier Corporation',
+        'brands': ['Carrier', 'Bryant'],
+        'founded': '1915',
+        'headquarters': 'Syracuse, New York, U.S.',
+        'keyPeople': ['Dave Gitlin'],
+        'website': 'https://carrier.com'
+    },
+    'goodman':{
+        'name': 'Goodman Manufacturing',
+        'brands': ['Amana', 'Goodman'],
+        'founded': '1975',
+        'headquarters': 'Houston, Texas, U.S.',
+        'keyPeople': ['Takeshi Ebisu'],
+        'website': 'https://www.goodmanmfg.com/'
+    },
+    'ingersol rand':{
+        'name': 'Ingersol Rand',
+        'brands': ['Ameristar', 'Oxbox'],
+        'founded': '1859',
+        'headquarters': 'Davidson, North Carolina, U.S.',
+        'keyPeople': ['Robert W. Gardner'],
+        'website': 'irco.com'
+    },
+    'international comfort products':{
+        'name': 'International Comfort Products',
+        'brands': ['Airquest', 'Arcoaire', 'Comfortmaker', 'Day & Night', 'Tempstar', 'Heil'],
+        'founded': '1979',
+        'headquarters': 'Lewisburg, Tennessee, U.S.',
+        'keyPeople': ['J.T. Holtschlag'],
+        'website': 'www.icpusa.com'
+    },
+    'lennox':{
+        'name': 'Lennox International',
+        'brands': ['Lennox', 'Armstrong', 'Ducane', 'Aire-flo'],
+        'founded': '1895',
+        'headquarters': 'Richardson, Texas, U.S.',
+        'keyPeople': ['Dave Lennox'],
+        'website': 'http://www.lennoxinternational.com'
+    },
+    'nortek':{
+        'name': 'Nortek Global',
+        'brands': ['Maytag', 'Frigidaire', 'Gibson', 'Nutone', 'Broan', 'Mammoth'],
+        'founded': '1919',
+        'headquarters': 'O\'Fallon, Missouri, U.S.',
+        'keyPeople': ['Dave LaGrand'],
+        'website': 'https://www.nortekhvac.com'
+    },
+    'rheem':{
+        'name': 'Rheem Manufacturing',
+        'brands': ['Rheem', 'Ruud', 'Weatherking'],
+        'founded': '1925',
+        'headquarters': 'Atlanta, Georgia, U.S.',
+        'keyPeople': ['Richard Rheem', 'Donald Rheem'],
+        'website': 'https://www.rheem.com'
+    },
+    'trane':{
+        'name': 'Trane Technologies',
+        'brands': ['Trane'],
+        'founded': '1871',
+        'headquarters': 'Swords, Ireland',
+        'keyPeople': ['Simon Ingersoll'],
+        'website': 'https://www.tranetechnologies.com'
+    },
+    'york':{
+        'name': 'York',
+        'brands': ['York','Coleman', 'Luxaire'],
+        'founded': '1874',
+        'headquarters': 'York, Pennsylvania, U.S.',
+        'keyPeople': ['Stephen Morgan Smith', 'Jacob Loucks', 'Henry LaMotte', 'Oliver Bollinger', 'George Buck', 'Robert Shetter'],
+        'website': 'https://www.york.com'
+    },
+    'unknown':{
+        'name': 'unknown',
+        'brands': [],
+        'founded': 'unknown',
+        'headquarters': 'unknown',
+        'keyPeople': 'unknown',
+        'website': 'unknown'
+    }
+}
+
+// @desc    Respond with the index.html file
+// @route   GET /
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+})
+
+// @desc    Respond with a JSON of all companies
+// @route   GET /api
+app.get('/api', (req, res) => {
+    res.json(manufacturers)
+})
+
+// @desc    Respond with a JSON of the specific company
+// @route   GET /api/:name
+app.get('/api/:name', (req, res) => {
+    const companyName = req.params.name.toLowerCase()
+    if(manufacturers[companyName]){
+        res.json(manufacturers[companyName])
+    }else{
+        res.json(manufacturers['unknown'])
+    }
+})
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
